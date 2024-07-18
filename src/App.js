1a769e6689ccf57cd2bf24bebefe0cde108b4c4a
Samarth Kumar <samarthk2402@gmail.com>
2024-07-18 20:51:28 +0100

    Fixing missing image bug

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ function App() {
   };
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <nav>
           <ul className="nav justify-content-end">
